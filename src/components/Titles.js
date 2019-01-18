6be5decdc78c1a0fa2ab99c38ea568c58b418117
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-01-19 02:30:00 +1100

    Changed Titles and Form components to be stateless functional components.

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,10 @@
 import React from "react";
 
-export class Titles extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Outside</h1>
-        <p>Find detailed weather statistics for your area.</p>
-      </div>
-    );
-  }
-}
+const Titles = () => (
+  <div>
+    <h1>Outside</h1>
+    <p>Find detailed weather statistics for your area.</p>
+  </div>
+);
 
 export default Titles;
