4955b4b0da1920aaf727dba27e7e348547056156
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-01-21 01:33:30 +1100

    added bootstrap links, started styling form, added container around components in app.js

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <Titles />
         <Form getWeather={this.getWeather} />
         <Weather
