2b77bda93e167cbb98dd9f5f4100cb64a990ce95
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-01-22 02:41:18 +1100

    created kelvinToFarenheit() function to convert temperature

--- src/App.js
+++ src/App.js
@@ -118,6 +118,9 @@ const translateTime = time => {
 const kelvinToCelcius = temperature => {
   return (temperature - 273.15).toFixed(1) + "°C";
 };
+const kelvinToFarenheit = temperature => {
+  return ((temperature - 273.15) * (9 / 5) + 32).toFixed(1) + "°F";
+};
 
 /*
     Function:   getImage
