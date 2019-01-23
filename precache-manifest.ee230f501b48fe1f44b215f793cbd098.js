f58abcd2d517296cb3790bc25ee26c28b16b2328
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-01-24 08:37:46 +1100

    Updates

---
+++ precache-manifest.ee230f501b48fe1f44b215f793cbd098.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49a2bcce45f3f6e76a77",
+    "url": "/outside/static/js/runtime~main.49a2bcce.js"
+  },
+  {
+    "revision": "35155e16e2e0fcc5bd03",
+    "url": "/outside/static/js/main.35155e16.chunk.js"
+  },
+  {
+    "revision": "babd94826ba891d51a02",
+    "url": "/outside/static/js/1.babd9482.chunk.js"
+  },
+  {
+    "revision": "35155e16e2e0fcc5bd03",
+    "url": "/outside/static/css/main.075e442c.chunk.css"
+  },
+  {
+    "revision": "87756a2ab14b5dd26a358059ec37c28e",
+    "url": "/outside/index.html"
+  }
+];
