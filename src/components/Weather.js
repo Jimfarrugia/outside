aafd1dfc176d568070c8501aa9129ec56e910621
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-01-24 09:05:47 +1100

    added temperatures are now returned with parseFloat when converted from kelvin. No more trailing zeroes and unnessessary decimals.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -32,10 +32,10 @@ export class Weather extends React.Component {
   };
 
   kelvinToCelcius = temperature => {
-    return (temperature - 273.15).toFixed(1);
+    return parseFloat((temperature - 273.15).toFixed(1));
   };
   kelvinToFarenheit = temperature => {
-    return ((temperature - 273.15) * (9 / 5) + 32).toFixed(1);
+    return parseFloat(((temperature - 273.15) * (9 / 5) + 32).toFixed(1));
   };
 
   render() {
