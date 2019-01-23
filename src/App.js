c0ffc9a801544a366053fb15d34651b9db1600ed
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-01-24 08:47:14 +1100

    API request now uses https -> http

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
     );
     const data = await api_call.json();
 
